54ec5b43ec34e7e792960db9010a8f185b42bda2
Every2 <christiansantoslima21@gmail.com>
2024-07-05 17:38:24 -0300

    feat: implement database connection

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use std::env;
 use axum::{routing::post, Router};
 use real_time_app::create_user;
+use sqlx::{migrate::MigrateDatabase, Sqlite};
 
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().expect("create .env file in root directory");
+    let url = env::var("DATABASE_URL").expect("ENV VARIABLE DOESN'T EXIST");
+    if !Sqlite::database_exists(&url).await.unwrap_or(false) {
+        println!("Creating Database...");
+        match Sqlite::create_database(&url).await {
+            Ok(_) => println!("Created db!"),
+            Err(error) => panic!("error {}", error),
+        }
+    } else {
+        println!("Database already exists");
+    }
     let app = Router::new()
         .route("/auth/register", post(create_user));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
